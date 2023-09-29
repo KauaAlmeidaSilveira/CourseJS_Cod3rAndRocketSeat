79273868b51b51ae3ad24a728f2e8bb3b8c75a2d
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-09-29 14:12:50 -0300

    javaExerciseMap

--- CourseJavaUdemy/CourseJava/src/GenericsSetMap/ExerciseSetCourse.java
+++ CourseJavaUdemy/CourseJava/src/GenericsSetMap/ExerciseSetCourse.java
@@ -6,7 +6,7 @@ import Model.Entities.Student;
 
 import java.util.*;
 
-public class ExerciseCourse {
+public class ExerciseSetCourse {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
